657d25fecea8bdbadcf08bb23457bc7819233da0
UltraProton <adityarksht64@gmail.com>
2019-10-03 22:12:30 +0530

    Added first test file

---
+++ test/sirtest.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+void fun(int n){
+    int y[n];
+
+    printf("%d ",y[0]);
+}
+
+int main(){
+    
+    char a[]="hello";
+    unsigned int x=0;
+
+    scanf("%d",&x);
+    
+    fun(x);
+
+    return 0;
+}
