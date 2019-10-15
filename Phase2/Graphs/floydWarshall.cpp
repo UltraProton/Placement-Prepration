e532dd2d11119a5497b4303e00d8d4e7c8d357ac
UltraProton <adityarksht64@gmail.com>
2019-10-15 23:24:30 +0530

    Added floyd warshall code

--- Phase2/Graphs/floydWarshall.cpp
+++ Phase2/Graphs/floydWarshall.cpp
@@ -19,6 +19,9 @@ int main(int argc, char const *argv[])
         }
         cout<<endl;
     }
+
+
+    floyd_warshall(G,0,2);
     
     return 0;
 }
@@ -26,21 +29,51 @@ int main(int argc, char const *argv[])
 
 
 void floyd_warshall(vector<vector<int>> &G, int src, int dest){
-    vector<vector<int>> temp=G;
-
     int n=G.size();
+    vector<vector<int>> dist(n,vector<int>(n,10000));
+    vector<vector<int>>path(n,vector<int>(n,-1));
+    
+    for(int i=0;i<n;i++){
+        dist[i][i]=0;
+        path[i][i]=i;
+    }
 
     for(int i=0;i<n;i++){
-        temp[i][i]=0;
+        for(int j=0;j<n;j++){
+            dist[i][j]=G[i][j];
+            path[i][j]=j;
+        }
     }
+
+
 
     int k=n; // no of vertices
 
     for(int v=1;v<=k;v++){
         for(int i=0;i<n;i++){
             for(int j=0;j<n;j++){
-                temp[i][j]= min(temp[i][j], temp[i][k]+temp[k][j]);
+                if(dist[i][j]> dist[i][k]+ dist[k][j]){
+                    dist[i][j]=dist[i][k]+dist[k][j];
+                    path[i][j]= path[i][k];
+                }
             }
+        }
+    }
+
+    cout<<"Print the matrix:"<<endl;
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            cout<<dist[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+
+
+    if(path[src][dest]!=-1){
+        cout<<src<<" ";
+        while(src!=dest){
+            src=path[src][dest];
+            cout<<src<<" ";
         }
     }
 }
