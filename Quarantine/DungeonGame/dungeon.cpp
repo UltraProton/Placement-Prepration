49b14b143d760c371e3c34fd1e9b622684ad9a18
UltraProton <cs18mtech11008@iith.ac.in>
2020-05-09 13:08:37 +0530

    Added input mechanism for matrix

--- Quarantine/DungeonGame/dungeon.cpp
+++ Quarantine/DungeonGame/dungeon.cpp
@@ -5,6 +5,17 @@ using namespace std;
 int dungeon(vector<vector<int>> &grid);
 
 int main(){
+    int t=0;
+    int m=0;
+    int n=0;
+    cin>>m>>n;
+    vector<vector<int>> grid(m,vector<int>(n,0));
+    for(int i=0;i<m;i++){
+        for(int j=0;j<n;j++){
+            cin>>grid[i][j];
+        }
+    }
+    
     return 0;
     
 }
